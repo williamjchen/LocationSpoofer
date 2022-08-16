0b3dd8a43a7b2c6db2eb7ec95d5d817812aa5ddb
William Chen <williamchenjian@gmail.com>
2022-08-15 20:20:20 -0400

    add arrow animations and button scale animation

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
 document.getElementById("toggleBtn").addEventListener("click", () => {
-  document.getElementById("toggleBtn").classList.toggle('active');
+  document.getElementById("toggleBtn").classList.toggle('active')
+
+  let status = document.getElementById("OnStatus")
+  status.innerHTML = status.innerHTML === "On"? "Off" : "On"
 });
